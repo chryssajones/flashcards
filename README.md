17beba325ff0f0e48814072f6ef68f2c8625a848
Chryssa <chryssajones@gmail.com>
2017-10-22 01:19:05 -0700

    I think I screwed it up worse than it was before.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # flashcards
 A command-line flashcard application using Node.js
+
+To start the app, run node cards.js
--- cards.js
+++ cards.js
@@ -8,34 +8,61 @@ var correct = 0;
 var incorrect = 0;
 var i = 1;
 
-console.log("Card Constructor is ready!");
+// console.log("I couldn't get the CLI to work so I'm running the app from this file instead.");
+console.log("Welcome to the flashcard practice app.");
 
-var deckChoice = spanish;
+var deckList = ["French", "Italian", "Hawaiian", "Spanish"];
+var deckChoice = "";
+
+var start = function() {
+	if (deckChoice == "") {
+		inquirer.prompt([
+	      {
+	        name: "choice",
+	        type: "list",
+	        choices: deckList,
+	        message: "Which deck would you like to practice?"
+	      }
+	    ]).then(function(answer) {
+	    	console.log("You are using the " + answer.choice + " deck. Good luck!")
+	    	var string = answer.choice;
+	    	deckChoice = string.toLowerCase();
+			start();
+			});
+	} else {
+		console.log(deckChoice);
+		getCard(deckChoice);
+	}
+
+ };
 
 // this is the card constructor
 var getCard = function(deck) {
-	console.log("The getCard function was called")
+	// console.log("The getCard function was called");
 	var c = Math.floor((Math.random() * deck.length));
-	console.log("Ready? Here's card #" + i);
+	console.log("Ready? Here's card #" + i + " on the " + deck + " deck:");
 	buildCard(deck, c);
 	// flipSide();
-	// console.log("side1 = " + side1 ", side2 = " + side2);	
+	// console.log("side1 = " + side1 ", side2 = " + side2);
 	query(this.front, this.back);
 	i++;
 };
 
 
 var buildCard = function(deck, i) {
-	console.log("The buildCard function was called on card #" + i);
+	console.log("The buildCard function was called on card #" + i + " of the " + deck + " deck.");
 	this.front = deck[i].front;
+	console.log(this.front);
 	this.back = deck[i].back;
-	return this.front, this.back;
+	console.log(this.back);
+	return (this.front, this.back);
 };
 
 
 var userGuess = "";
 var query = function(side1, side2){
-	console.log("The query function was called");
+	console.log(side1 + " | " + side2);
+	// console.log("The query function was called");
 	inquirer.prompt([
       {
         name: "input",
@@ -45,8 +72,8 @@ var query = function(side1, side2){
     ]).then(function(answer) {
     	userGuess = answer.input;
     	if (userGuess == side2) {
+ 			correct ++;   		
 			console.log("That is correct! Total correct:" + correct);
-			correct ++;
 			if (correct < 10) {
 				getCard(deckChoice);
 			} else if (correct >= 10) {
@@ -55,8 +82,8 @@ var query = function(side1, side2){
 			}
 
 		} else {
-			console.log("Sorry, that is incorrect. The correct answer is " + side2);
-			incorrect ++;
+			incorrect ++;			
+			console.log("Sorry, that is incorrect. The correct answer is " + side2 + ". You have " + incorrect + " incorrect answers.");
 			if (incorrect < 3) {
 				getCard(deckChoice);
 			} else if (incorrect >= 3) {
@@ -67,25 +94,27 @@ var query = function(side1, side2){
 };
 
 
-var side1 = "";
-var side2 = "";
-var flipSide = function() {
-	var r = Math.floor((Math.random() * 2));
-	console.log("The flipSide function was called and the random number is " + r);
-		if (r == 0) {
-			side1 = "front";
-			side2 = "back";
-		} else {
-			side1 = "back";
-			side2 = "front";
-		}
-		return side1, side2;
-};
+// var side1 = "";
+// var side2 = "";
+// var flipSide = function() {
+// 	var r = Math.floor((Math.random() * 2));
+// 	// console.log("The flipSide function was called and the random number is " + r);
+// 		if (r == 0) {
+// 			side1 = "front";
+// 			side2 = "back";
+// 		} else {
+// 			side1 = "back";
+// 			side2 = "front";
+// 		}
+// 		return side1, side2;
+// };
 
 
-getCard(deckChoice);
+// getCard(deckChoice);
 
 // module.exports = buildCard;
-module.exports = getCard;
+// module.exports = getCard;
 // module.exports = correct;
 // module.exports = incorrect;
+
+start();
